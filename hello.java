7e281c772928a2bc0a4e83020e3995a67e0e1aef
iniesta2014 <1029127930@qq.com>
2015-07-19 15:28:33 +0800

    first_edition_master_branch

--- hello.java
+++ hello.java
@@ -1,11 +1,8 @@
 
-public static  void main{
-int i=1;
-for(i=1;i<3;i++)
-{
-System.out.println("hello java,hello git");
+public static void main {
+int i;
+for (i=1;i<3;i++){
 System.out.println("This is a master branch");
-
+}
 }
 
-}
